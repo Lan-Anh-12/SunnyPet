a0461f3b3336221dd09ceb90722ec76d9920d4b5
Lan-Anh-12 <lzltop1lzl@gmail.com>
2026-03-30 09:47:04 +0700

    Add comment for data insertion order
    Added a comment to ensure data integrity during insertion.

---
+++ SQL/sample_data.sql
@@ -0,0 +1,2 @@
+/*Để đảm bảo tính toàn vẹn dữ liệu (không bị lỗi khóa ngoại),
+cần thêm dữ liệu theo đúng thứ tự: Tài khoản -> Người dùng (Admin/Bác sĩ/Khách) -> Danh mục (Dịch vụ/Thuốc) -> Thú cưng -> Lịch hẹn -> Bệnh án.*/
